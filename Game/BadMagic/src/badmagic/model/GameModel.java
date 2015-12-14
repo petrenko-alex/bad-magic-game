d0d03f0008b287b01f80e2a56fdb7694f3c541d5
Александр Петренко <gafk555@gmail.com>
2015-12-14 23:29:30 +0300

    Начата реализация модели.

--- Game/BadMagic/src/badmagic/model/GameModel.java
+++ Game/BadMagic/src/badmagic/model/GameModel.java
@@ -1,6 +1,32 @@
 package badmagic.model;
 
+import badmagic.model.Level;
+import java.util.ArrayList;
 
 public class GameModel {
+    
+    public GameModel() {
+        
+        loadLevels();
+    }
+    
+    public void loadLevels() {
+        
+        String levelFile = "src/badmagic/resources/levels/MysteryRectangle.json";
+        
+        
+        Level level = new Level(levelFile);
+        _levels.add(level);
+        
+        
+        _field = level.getField();
+        int a = 32;
+    }
+    
+    
+    private GameField _field;
+    private ArrayList<Level> _levels = new ArrayList();
+    private static final String PATH_TO_LEVELS_INFO_FILE = 
+                          "src/badmagic/resources/levels/levelsinfo.json";
     
 }
