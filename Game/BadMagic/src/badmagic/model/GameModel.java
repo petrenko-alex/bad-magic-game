54b73d8e921c4e9329796d02797b859d1d6f7885
Александр Петренко <gafk555@gmail.com>
2015-12-17 18:48:38 +0300

    В класс GameModel добавлен метод, возвращающий поле.

--- Game/BadMagic/src/badmagic/model/GameModel.java
+++ Game/BadMagic/src/badmagic/model/GameModel.java
@@ -14,15 +14,17 @@ public class GameModel {
         
         String levelFile = "src/badmagic/resources/levels/MysteryRectangle.json";
         
-        
         Level level = new Level(levelFile);
         _levels.add(level);
-        
         
         _field = level.getField();
         int a = 32;
     }
     
+    public GameField getField() {
+        
+        return _field;
+    }
     
     private GameField _field;
     private ArrayList<Level> _levels = new ArrayList();
