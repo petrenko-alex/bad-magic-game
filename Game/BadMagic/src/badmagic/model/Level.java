1d6bc6d4c66042887c6b3ec0d7247f45f72c29a1
Александр Петренко <gafk555@gmail.com>
2015-12-18 12:22:10 +0300

    В классе Level выделены вспомогательные функции считывания и добавлены проверки корректности данных.

--- Game/BadMagic/src/badmagic/model/Level.java
+++ Game/BadMagic/src/badmagic/model/Level.java
@@ -17,146 +17,194 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import badmagic.model.gameobjects.GameObjectsFactory;
 import java.util.Set;
-     
+import java.util.logging.Logger;
+
 
 public class Level {
-    
+
     Level(String levelPath) {
-        
+
         loadLevel(levelPath);
     }
-     
+
     public void loadLevel(String levelPath) {
-        
+
+        _levelPath = levelPath;
         Point size;
         JSONParser parser = new JSONParser();
-        
+
         try {
 
             Object object = parser.parse(new FileReader(levelPath));
             JSONObject json = (JSONObject) object;
-            
+
             /* Название уровня */
             if(json.containsKey("LevelName")) {
-                
+
                 _name = (String)json.get("LevelName");
             }
-            
+
             /* История уровня */
             if(json.containsKey("LevelHistory")) {
-                
+
                 _history = (String)json.get("LevelHistory");
             }
-            
+
             /* Количество ходов игрока */
             if(json.containsKey("PlayerMoves")) {
-                
+
                 _moves = Integer.parseInt(json.get("PlayerMoves").toString());
             }
-            
+
             /* Размер поля */
-            if( json.containsKey("FieldSize")) {
-                
-                JSONArray tmp = (JSONArray)json.get("FieldSize");
-                int width = Integer.parseInt(tmp.get(0).toString());
-                int height = Integer.parseInt(tmp.get(1).toString());
-                _field = new GameField(width,height);
-                
-            } else {
-                
-                Exception e = new Exception( 
-                                        "Не задан размер поля в файле уровня \"" 
-                                        + levelPath + "\"");
-                throw e;
-            }
-            
+            parseFieldSize(json);
+
             /* Объекты поля */
-            if( json.containsKey("FieldObjects")) {
-                
-                JSONArray objects = (JSONArray)json.get("FieldObjects");
-                parseObjects(objects); 
-                
-            } else {
-                
-                Exception e = new Exception( 
-                                      "Не заданы объекты поля в файле уровня \"" 
-                                      + levelPath + "\"");
-                
-            }
+            parseObjects(json);
+
         } catch (Exception e) {
-            
+
             e.printStackTrace();
-            BadMagic.log.info("Ошика при чтении файда \"" + levelPath + "\"");
+            BadMagic.log.info("Ошика при разборе файла \"" + levelPath + "\"");
         }
     }
 
     public int getMoves() {
-        
+
         return _moves;
     }
 
     public GameField getField() {
-        
+
         return _field;
     }
 
     public String getName() {
-        
+
         return _name;
     }
 
     public String getHistory() {
-        
+
         return _history;
     }
 
-    private void parseObjects(JSONArray objects) {
-        
-        /* Парсинг объектов в массиве objects */
-        for(Object i : objects) {
-            
-            JSONObject obj = (JSONObject)i;
-            Set keys = obj.keySet();
-            
-            /* 
-             * Ключ - конкретный тип объекта, 
-             * должен соответствовать имени класса 
-             */
-            for(Object key : keys) {
-                
-                JSONObject tmp = (JSONObject) obj.get(key.toString());
-                parseObject(tmp,key.toString());
+    private void parseFieldSize(JSONObject json) throws Exception {
+
+        if ( json.containsKey("FieldSize") ) {
+
+            JSONArray tmp = (JSONArray) json.get("FieldSize");
+            int width = Integer.parseInt(tmp.get(0).toString());
+            int height = Integer.parseInt(tmp.get(1).toString());
+
+            if ( width > MAX_WIDTH || height > MAX_HEIGHT ) {
+
+                Exception ex = new Exception("Размер поля ("
+                                             + tmp.get(0).toString() + ";"
+                                             + tmp.get(1).toString()
+                                             + ") превышет "
+                                             + "максимально допустимый ("
+                                             + MAX_WIDTH + ";" + MAX_HEIGHT + ")");
+                throw ex;
+
+            } else if ( width < 1 || height < 1) {
+
+                Exception ex = new Exception("Размер поля ("
+                                             + tmp.get(0).toString() + ";"
+                                             + tmp.get(1).toString()
+                                             + ") не допустим");
+                throw ex;
             }
+
+            _field = new GameField(width, height);
+
+        } else {
+
+            Exception ex = new Exception("Не задан размер поля в файле уровня");
+            throw ex;
         }
-        
     }
-    
-    private void parseObject(JSONObject obj,String objClassName) {
-        
+
+    private void parseObjects(JSONObject json) throws Exception {
+
+        if ( json.containsKey("FieldObjects") ) {
+
+            JSONArray objects = (JSONArray) json.get("FieldObjects");
+
+            /* Парсинг объектов в массиве objects */
+            for ( Object i : objects ) {
+
+                JSONObject obj = (JSONObject) i;
+                Set keys = obj.keySet();
+
+                /*
+                 * Ключ - конкретный тип объекта,
+                 * должен соответствовать имени класса
+                 */
+                for ( Object key : keys ) {
+
+                    JSONObject tmp = (JSONObject) obj.get(key.toString());
+                    parseObject(tmp, key.toString());
+                }
+            }
+        } else {
+
+            Exception ex = new Exception("Не заданы объекты поля в файле уровня");
+            throw ex;
+        }
+    }
+
+    private void parseObject(JSONObject obj,String objClassName) throws Exception {
+
         /* Парсинг объекта obj с типом, заданным именем objName */
         if( obj.containsKey("Positions")) {
-                    
+
             JSONArray pos = (JSONArray)obj.get("Positions");
-            
+
             /* Позиции объектов данного типа */
             for(Object j : pos) {
-                        
+
                 JSONArray onePos = (JSONArray) j;
                 int x = Integer.parseInt(onePos.get(0).toString());
                 int y = Integer.parseInt(onePos.get(1).toString());
-                
+
+                if(!_field.isPositionUnique(new Point(x,y))) {
+
+                    String error = "Объект типа " + objClassName +
+                                   " с позицией (" + onePos.get(0).toString() +
+                                   ";" + onePos.get(1).toString() +
+                                   ") не может быть размещен, "
+                                   + "т.к. его позиция уже занята.";
+
+                    throw new Exception(error);
+
+                } else if((x < 1 || x > _field.getWidth()) ||
+                          (y < 1 || y > _field.getHeight())) {
+
+                    String error = "Объект типа " + objClassName +
+                                   " с позицией (" + onePos.get(0).toString() +
+                                   ";" + onePos.get(1).toString() +
+                                   ") не может быть размещен, "
+                                   + "т.к. его позиция за пределами поля.";
+
+                    throw new Exception(error);
+                }
+
                 /* Создаем объект с помощью фабрики */
                 GameObject object = new GameObjectsFactory().createGameObject(objClassName, getField());
-                        
                 getField().addObject( new Point(x,y), object);
-                        
-            }            
+
+            }
         }
     }
-   
+
     ////////////////////////////// Данные /////////////////////////////////////
+    private static final int MAX_WIDTH = 16;
+    private static final int MAX_HEIGHT = 11;
+
     private int _moves = 100;
     private GameField _field;
     private String _name = "default level name";
     private String _history = "default level history";
+    private String _levelPath;
 }
