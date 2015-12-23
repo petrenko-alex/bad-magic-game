79a99f888ba2accb44a203d787cc814a195015f2
Александр Петренко <gafk555@gmail.com>
2015-12-23 20:47:18 +0300

    Добавлен внутренный класс слушателя событий игровых объектов.

--- Game/BadMagic/src/badmagic/view/GamePanel.java
+++ Game/BadMagic/src/badmagic/view/GamePanel.java
@@ -2,7 +2,9 @@ package badmagic.view;
 
 import badmagic.model.GameModel;
 import badmagic.BadMagic;
+import badmagic.events.GameObjectListener;
 import badmagic.model.gameobjects.GameObject;
+import badmagic.navigation.Direction;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -17,6 +19,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.EventObject;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Level;
@@ -71,35 +74,35 @@ public class GamePanel extends JPanel {
     }
 
     private void paintGrid(Graphics g) {
-        
+
         /* Координаты и размеры */
         int rows = _model.getField().getHeight();
         int columns = _model.getField().getWidth();
         int height = rows * CELL_SIZE;
         int width = columns * CELL_SIZE;
-        
+
         int areaWidth = BadMagic.getWindowWidth() - INFO_PANEL_WIDTH;
         int areaHeight = BadMagic.getWindowHeight();
         int xOffset = (areaWidth - width) / 2;
         int yOffset = (areaHeight - height) / 2;
-        
+
         _fieldStartX = INFO_PANEL_WIDTH + xOffset;
         _fieldStartY = yOffset;
-        
+
         /* Обрамляющий прямоугольник */
         g.drawRect(_fieldStartX,_fieldStartY,width,height);
-        
+
         /* Текстуры */
         for(int i = 1; i < (rows + 1);i++) {
-            
+
             int y = _fieldStartY + CELL_SIZE * (i-1);
             for(int j = 1; j < (columns + 1);j++) {
-                
+
                 int x = _fieldStartX + CELL_SIZE * (j - 1);
                 g.drawImage(_cellPic, x, y, null);
             }
         }
-        
+
         /* Сетка */
         /* Вертикальные линии */
         for(int i = 1; i <= (columns + 1); i++)
@@ -107,36 +110,45 @@ public class GamePanel extends JPanel {
             int x1 = _fieldStartX + CELL_SIZE * (i - 1);
             g.drawLine(x1, _fieldStartY, x1, (_fieldStartY + height));
         }
-        
-        /* Горизонтальные линии */ 
+
+        /* Горизонтальные линии */
         for(int i = 1; i <= (rows + 1); i++)
         {
             int y1 = _fieldStartY + CELL_SIZE * (i - 1);
             g.drawLine(_fieldStartX, y1, (_fieldStartX + width), y1);
         }
     }
-    
+
     private void paintObjects(Graphics g) {
-        
+
         for(GameObject obj : _model.getField().getObjects()) {
-            
+
             Point objPos = obj.getPosition();
             objPos = getPanelPosition(objPos);
             obj.paint(g, objPos);
         }
     }
-    
+
     private void loadPic() {
-        
+
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         _cellPic = toolkit.getImage(PIC);
     }
-    
+
     private Point getPanelPosition(Point logPos) {
-    
+
         int x = _fieldStartX + (CELL_SIZE * (logPos.x - 1));
         int y = _fieldStartY + (CELL_SIZE * (logPos.y - 1));
         return new Point(x,y);
+    }
+
+    private class ObjectsObserver implements GameObjectListener {
+
+        @Override
+        public void objectChanged(EventObject e) {
+
+            ((GameObject)e.getSource()).paint(getGraphics(), null);
+        }
     }
 
     private class KeyHandler extends KeyAdapter {
@@ -146,68 +158,68 @@ public class GamePanel extends JPanel {
 
             int key = _ke.getKeyCode();
             if (_ke.isControlDown()) {
-                
+
                 /* Переместить объект */
                 switch (_ke.getKeyCode()) {
-                    
+
                     case (KeyEvent.VK_LEFT): {
-                        
+
                         BadMagic.log.info("Попытка сдвинуть стол влево");
                         break;
                     }
                     case (KeyEvent.VK_RIGHT): {
-                        
+
                         BadMagic.log.info("Попытка сдвинуть стол вправо");
                         break;
                     }
                     case (KeyEvent.VK_UP): {
-                        
+
                         BadMagic.log.info("Попытка сдвинуть стол вверх");
                         break;
                     }
                     case (KeyEvent.VK_DOWN): {
-                        
+
                         BadMagic.log.info("Попытка сдвинуть стол вниз");
                         break;
                     }
                     default: {
-                        
+
                         BadMagic.log.info("Неизвестное действие");
                         break;
                     }
                 }
             } else {
-               
-                /* Переместить игрока */ 
+
+                /* Переместить игрока */
                 switch (_ke.getKeyCode()) {
-                    
+
                     case (KeyEvent.VK_LEFT): {
-                        
-                        BadMagic.log.info("Переместиться влево");
+
+                        _model.getPlayer().move(Direction.west());
                         break;
                     }
                     case (KeyEvent.VK_RIGHT): {
-                        
-                        BadMagic.log.info("Переместиться вправо");
+
+                        _model.getPlayer().move(Direction.east());
                         break;
                     }
                     case (KeyEvent.VK_UP): {
-                        
-                        BadMagic.log.info("Переместиться вверх");
+
+                        _model.getPlayer().move(Direction.north());
                         break;
                     }
                     case (KeyEvent.VK_DOWN): {
-                        
-                        BadMagic.log.info("Переместиться вниз");
+
+                        _model.getPlayer().move(Direction.south());
                         break;
                     }
                     case (KeyEvent.VK_SPACE): {
-                        
+
                         BadMagic.log.info("Активировать объект");
                         break;
                     }
                     default: {
-                        
+
                         BadMagic.log.info("Неизвестное действие");
                         break;
                     }
@@ -217,7 +229,7 @@ public class GamePanel extends JPanel {
         }
 
     }
-    
+
     private GameModel _model;
 
     private static final Color BACKGROUND_COLOR = new Color(47, 79, 79);
@@ -228,9 +240,9 @@ public class GamePanel extends JPanel {
     private static final String FONT_TYPE = "Comic Sans MS";
     private static final Color FONT_COLOR = new Color(205, 133, 63);
     private static final String PIC = "src/badmagic/resources/brick.png";
-    
+
     private static final int CELL_SIZE = 64;
-    
+
     private int _fieldStartX;
     private int _fieldStartY;
     private Image _cellPic;
