620017720f2c544920235ac011128a16e603991f
Александр Петренко <gafk555@gmail.com>
2015-12-23 19:15:02 +0300

    В класс Player добавлен метод move.

--- Game/BadMagic/src/badmagic/model/gameobjects/Player.java
+++ Game/BadMagic/src/badmagic/model/gameobjects/Player.java
@@ -1,6 +1,7 @@
 package badmagic.model.gameobjects;
 
 import badmagic.model.GameField;
+import badmagic.navigation.Direction;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Toolkit;
@@ -13,18 +14,27 @@ public class Player extends GameObject {
         loadPic();
     }
 
+    public void move(Direction moveDirection) {
+
+        if( _field.isNextPosEmpty(_position,moveDirection) ) {
+
+            _position = _field.getNextPos(_position,moveDirection);
+        }
+    }
+
     @Override
     public void paint(Graphics g, Point pos) {
-        
+
         g.drawImage(_image, pos.x, pos.y, null);
     }
-    
+
     @Override
     protected void loadPic() {
-        
+
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         _image = toolkit.getImage(PIC);
     }
-    
+
+    private Direction gazeDirection;
     private static final String PIC = "src/badmagic/resources/goat.png";
 }
