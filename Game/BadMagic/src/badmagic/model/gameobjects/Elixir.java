6399d909c71c139f0c472de72f7d02bd5caee767
Александр Петренко <gafk555@gmail.com>
2015-12-23 22:02:16 +0300

    В классах Elixir, Wall, WoodenTable изменен способ загрузки изображения: не привязано к абсолютному пути.

--- Game/BadMagic/src/badmagic/model/gameobjects/Elixir.java
+++ Game/BadMagic/src/badmagic/model/gameobjects/Elixir.java
@@ -3,7 +3,8 @@ package badmagic.model.gameobjects;
 import badmagic.model.GameField;
 import java.awt.Graphics;
 import java.awt.Point;
-import java.awt.Toolkit;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 
 public class Elixir extends CollectableObject {
@@ -15,16 +16,22 @@ public class Elixir extends CollectableObject {
 
     @Override
     public void paint(Graphics g, Point pos) {
-        
+
         g.drawImage(_image, pos.x, pos.y, null);
     }
-    
+
     @Override
     protected void loadPic() {
-        
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        _image = toolkit.getImage(PIC);
+
+        try {
+
+            _image = ImageIO.read(getClass().getResource(PIC));
+
+        } catch ( IOException ex ) {
+
+            ex.printStackTrace();
+        }
     }
-    
-    private static final String PIC = "src/badmagic/resources/Elixir.png";
+
+    private static final String PIC = "/badmagic/resources/Elixir.png";
 }
