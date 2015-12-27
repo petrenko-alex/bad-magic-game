516d2d39cbc3253388ffdb9694702c895a3cb94f
Александр Петренко <gafk555@gmail.com>
2015-12-27 15:25:19 +0300

    Изменен интерфейс слушаетля событий меню.

--- Game/BadMagic/src/badmagic/events/MenuListener.java
+++ Game/BadMagic/src/badmagic/events/MenuListener.java
@@ -1,12 +1,10 @@
 package badmagic.events;
 
-import java.awt.Event;
 import java.util.EventListener;
-import java.util.EventObject;
-
 
 public interface MenuListener extends EventListener {
-    
-    public void startCareerClicked(EventObject e);
-    public void continueCareerClicked(EventObject e);
+
+    public void startCareerClicked(MenuEvent e);
+    public void continueCareerClicked(MenuEvent e);
+    public void levelChoosen(MenuEvent e);
 }
