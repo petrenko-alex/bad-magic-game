68e40fc049577960b414bcf116a40743e46904d8
Александр Петренко <gafk555@gmail.com>
2016-01-04 19:28:09 +0300

    В класс сундука добавлены методы его отрисовки и загрузки его изображения.

--- Game/BadMagic/src/badmagic/model/gameobjects/Chest.java
+++ Game/BadMagic/src/badmagic/model/gameobjects/Chest.java
@@ -4,22 +4,47 @@ package badmagic.model.gameobjects;
 import badmagic.model.GameField;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 
 public class Chest extends MovableObject {
 
     public Chest(GameField field) {
         super(field);
+        loadPic();
     }
 
+    /**
+     * Метод отрисовки объекта.
+     *
+     * @param g среда отрисовки.
+     * @param pos позиция отрисоки.
+     */
     @Override
     public void paint(Graphics g, Point pos) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+
+        g.drawImage(_image, pos.x, pos.y, null);
     }
 
+    /**
+     * Метод загрузки изображения объекта.
+     */
     @Override
     protected void loadPic() {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        try {
+
+            _image = ImageIO.read(getClass().getResource(PIC));
+
+        } catch ( IOException ex ) {
+
+            ex.printStackTrace();
+        }
     }
-    
+
+    ///////////////////////////// Данные //////////////////////////////////////
+
+    /** Путь к файлу с изображением */
+    private static final String PIC = "/badmagic/resources/Chest.png";
 }
