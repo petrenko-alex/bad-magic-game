c324893e238dd3feb58fb81a3616509417f85131
Александр Ляшенко <stillnoash@gmail.com>
2016-01-06 13:11:47 +0300

    Реализованы методы для неподвижного объекта "Колонна"

--- Game/BadMagic/src/badmagic/model/gameobjects/Column.java
+++ Game/BadMagic/src/badmagic/model/gameobjects/Column.java
@@ -4,6 +4,8 @@ package badmagic.model.gameobjects;
 import badmagic.model.GameField;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 
 public class Column extends UnmovableObject {
@@ -14,12 +16,25 @@ public class Column extends UnmovableObject {
 
     @Override
     public void paint(Graphics g, Point pos) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        g.drawImage(_image, pos.x, pos.y, null);
     }
 
     @Override
     protected void loadPic() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+         try {
+
+            _image = ImageIO.read(getClass().getResource(PIC));
+
+        } catch ( IOException ex ) {
+
+            ex.printStackTrace();
+        }
     }
+    
+     ///////////////////////////// Данные //////////////////////////////////////
+
+    /** Путь к файлу с изображением */
+    private static final String PIC = "/badmagic/resources/column.png";
     
 }
