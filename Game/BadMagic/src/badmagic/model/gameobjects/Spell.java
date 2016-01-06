47728c2c247f356691001f58abb676054e32263c
Александр Ляшенко <stillnoash@gmail.com>
2016-01-06 13:18:34 +0300

    Реализованы методы для собираемого объекта "Заклинание"

--- Game/BadMagic/src/badmagic/model/gameobjects/Spell.java
+++ Game/BadMagic/src/badmagic/model/gameobjects/Spell.java
@@ -3,21 +3,46 @@ package badmagic.model.gameobjects;
 import badmagic.model.GameField;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 
 public class Spell  extends CollectableObject  {
-
+    /** Идентификатор открываемого объекта */
+    private int id = 0;
+    
+    public void setId (int _id){
+        id = _id;
+    }
+    
+    public int getId (int _id){
+        return id;
+    }
+    
     public Spell(GameField field) {
         super(field);
     }
 
     @Override
     public void paint(Graphics g, Point pos) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        g.drawImage(_image, pos.x, pos.y, null);
     }
 
     @Override
     protected void loadPic() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+         try {
+
+            _image = ImageIO.read(getClass().getResource(PIC));
+
+        } catch ( IOException ex ) {
+
+            ex.printStackTrace();
+        }
     }
+    
+     ///////////////////////////// Данные //////////////////////////////////////
+
+    /** Путь к файлу с изображением */
+    private static final String PIC = "/badmagic/resources/spell.gif";
 }
